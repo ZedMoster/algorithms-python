ccca7fa7535e791aa7ad24820c7782f369e7d278
xuml201 <1505636116@qq.com>
2020-09-16 15:41:53 +0800

    时间复杂度更新

--- 1.2选择排序.py
+++ 1.2选择排序.py
@@ -8,7 +8,7 @@ from cal_time import *
 def select_sort(li):
     '''时间复杂度：O(n2)'''
     for i in range(len(li) - 1):                  # 排序趟数 = 列表长度-1
-        min_loc = i                               # 待排序区域 默认最小值为第一个数
+        min_loc = i                               # 待排序区域 默认最小值为左侧第一个位置的数字 [0]
         for j in range(min_loc + 1, len(li)):     # 排序区
             if li[j] < li[min_loc]:               # 判断当前值是否小于初始值
                 min_loc = j                       # 初始值 == 当前值 定位
--- 2.1快速排序.py
+++ 2.1快速排序.py
@@ -5,14 +5,13 @@
 import random
 from cal_time import *
 
-# 列表中左侧值归位
+# 列表中最左侧值归位。分割成两段列表
 def partition(li, left, right):
     temp = li[left]                                  # 默认开始提取最左侧的值
     while left < right:                              # 左右区间范围指针
         while li[right] >= temp and left < right:    # 从右往左找比temp小的数
             right -= 1                               # 范围向左侧缩小一步
         li[left] = li[right]                         # 把右侧的值写入到左侧空位
-
         while li[left] <= temp and left < right:     # 从左往右找比temp小的数
             left += 1                                # 范围向右侧缩小一步
         li[right] = li[left]                         # 把左侧的值写入到右侧空位
@@ -22,10 +21,10 @@ def partition(li, left, right):
 
 def quick_sort(li, left, right):
     '''时间复杂度：O(nlogn)'''
-    if left < right:                       # 至少右两个元素
-        mid = partition(li, left, right)   # 归位最左侧元素
-        quick_sort(li, left, mid-1)        # 归位元素 左侧区域
-        quick_sort(li, mid+1, right)       # 归位元素 右侧区域
+    if left < right:                                 # 至少右两个元素
+        mid = partition(li, left, right)             # 归位最左侧元素
+        quick_sort(li, left, mid-1)                  # 归位元素 左侧区域
+        quick_sort(li, mid+1, right)                 # 归位元素 右侧区域
 
 @cal_time
 def quickSort(li, left, right):
